42e870e57509dcbe99a8390fa65eb4323fc82247
Neha Ujjwal <neha.ujjwal@scaler.com>
2025-04-10 04:12:20 +0530

    fix: styles

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,34 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+const isProd = process.env.NODE_ENV === "production";
+const ASSET_PREFIX = isProd ? process.env.NEXT_PUBLIC_CDN_HOST : "";
+
 const nextConfig: NextConfig = {
+  reactStrictMode: false, //todo: remove for prod
+  trailingSlash: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   sassOptions: {
     includePaths: [path.resolve(__dirname, "src/styles")],
     prependData: `@import "@/styles/base.scss";`,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cloudfront.net',
+      },
+    ],
+  },  
+  assetPrefix: ASSET_PREFIX,
 };
 
 export default nextConfig;
