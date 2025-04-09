2e6343b4e981473cc4c1c906edb61504d4a415ca
sharma.sahil <sharma.sahil@scaler.com>
2025-04-09 15:14:12 +0530

    fix(config): Testing Config

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,11 @@
 import type { NextConfig } from "next";
 import path from "path";
 
-const isProd = process.env.NODE_ENV === "production";
-const ASSET_PREFIX = isProd ? process.env.NEXT_PUBLIC_CDN_HOST : "";
-
 const nextConfig: NextConfig = {
-  reactStrictMode: false, //todo: remove for prod
-  trailingSlash: true,
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
   sassOptions: {
     includePaths: [path.resolve(__dirname, "src/styles")],
     prependData: `@import "@/styles/base.scss";`,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-      },
-      {
-        protocol: 'https',
-        hostname: 'd1t59tgpzgv8ca.cloudfront.net',
-      },
-    ],
-  },  
-  assetPrefix: ASSET_PREFIX,
 };
 
 export default nextConfig;
