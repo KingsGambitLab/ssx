a7f3427560b48745e9fa3992e9986ef9e559ced2
sharma.sahil <sharma.sahil@scaler.com>
2025-04-10 21:57:09 +0530

    fix(infra): Amplifyyml

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,12 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false, //todo: remove for prod
   trailingSlash: true,
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
+  basePath: "/",
+  assetPrefix: "/ssx-next-js-assets",
   sassOptions: {
     includePaths: [path.resolve(__dirname, "src/styles")],
     prependData: `@import "@/styles/base.scss";`,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.cloudfront.net',
-      },
-    ],
   },
 };
 
