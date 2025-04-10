a555f116f80556bea6765bc5f2339876335743f2
Aayush Kumar Shrivastava <aayush.shrivastava@scaler.com>
2025-04-10 17:15:49 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const ASSET_PREFIX = isProd ? process.env.NEXT_PUBLIC_CDN_HOST : "";
 const nextConfig: NextConfig = {
   reactStrictMode: false, //todo: remove for prod
   trailingSlash: true,
+  output: "export",
   logging: {
     fetches: {
       fullUrl: true,
