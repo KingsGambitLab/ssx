8bfb2f02c07d747568a135687eae6b3028c8da06
Om Mishra <om.mishra@scaler.com>
2025-04-30 18:27:45 +0530

    add(SST): Degree Hero

--- src/app/(sst)/school-of-technology/degree/page.tsx
+++ src/app/(sst)/school-of-technology/degree/page.tsx
@@ -1,13 +1,18 @@
-import SstVsTraditional from "@modules/sst/degree/ui/SstVsTraditional";
+import SstVsTraditional from '@modules/sst/degree/ui/SstVsTraditional';
 import KeyFeatures from "@modules/sst/degree/ui/KeyFeatures";
 import styles from "./page.module.scss";
+import DegreeHero from "@modules/sst/degree/components/DegreeHero/DegreeHero";
+
 
 export default function Page() {
 
   return (
+
     <div className={styles.container}>
+      <DegreeHero />
       <SstVsTraditional />
       <KeyFeatures />
     </div>
+
   );
 }
