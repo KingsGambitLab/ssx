b95977b574b3e4e7dad2b392e1617b6cf7836b6b
Aayush Kumar Shrivastava <aayush.shrivastava@scaler.com>
2025-05-02 00:28:24 +0530

    section padding fixes

--- src/app/(sst)/school-of-technology/degree/page.tsx
+++ src/app/(sst)/school-of-technology/degree/page.tsx
@@ -4,7 +4,7 @@ import KeyFeatures from "@modules/sst/degree/ui/KeyFeatures";
 import DegreeFaq from "@modules/sst/degree/ui/DegreeFaq";
 
 import styles from "./page.module.scss";
-import DegreeHero from "@modules/sst/degree/components/DegreeHero/DegreeHero";
+import DegreeHero from "@modules/sst/degree/ui/DegreeHero";
 
 
 export default function Page() {
