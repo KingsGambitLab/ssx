6e7ac6257eac6e2929f6ba44e92a559c6a364ffa
Aayush Kumar Shrivastava <aayush.shrivastava@scaler.com>
2025-05-02 02:27:55 +0530

    added apply cta banner and faq section

--- src/app/(sst)/school-of-technology/career-outcomes/page.tsx
+++ src/app/(sst)/school-of-technology/career-outcomes/page.tsx
@@ -1,9 +1,13 @@
+import Faq from "@modules/sst/career-outcomes/ui/Faq";
+import ApplyCtaBanner from "@components/Sst/ApplyCtaBanner";
 import styles from "./page.module.scss";
 
 export default function Page() {
 
   return (
-    <>
-    </>
+    <div className={styles.container}>
+      <ApplyCtaBanner />
+      <Faq />
+    </div>
   );
 }
