39c458debe69d15cd78f7963f90fb9036367e00e
Prem Shah <prem.shah@scaler.com>
2025-05-02 14:34:12 +0530

    nit

--- src/app/(sst)/school-of-technology/degree/page.tsx
+++ src/app/(sst)/school-of-technology/degree/page.tsx
@@ -1,12 +1,12 @@
 import ApplyCtaBanner from "@components/Sst/ApplyCtaBanner";
-import SstVsTraditional from "@modules/sst/degree/ui/SstVsTraditional";
-import KeyFeatures from "@modules/sst/degree/ui/KeyFeatures";
+import DegreeHero from "@modules/sst/degree/ui/DegreeHero";
 import DegreeFaq from "@modules/sst/degree/ui/DegreeFaq";
+import DegreePathways from "@modules/sst/degree/ui/DegreePathways";
+import KeyFeatures from "@modules/sst/degree/ui/KeyFeatures";
+import SkillsVsDegree from "@modules/sst/degree/ui/SkillsVsDegree/SkillsVsDegree";
+import SstVsTraditional from "@modules/sst/degree/ui/SstVsTraditional";
 
 import styles from "./page.module.scss";
-import DegreePathways from "@modules/sst/degree/ui/DegreePathways";
-import SkillsVsDegree from "@modules/sst/degree/ui/SkillsVsDegree/SkillsVsDegree";
-import DegreeHero from "@modules/sst/degree/ui/DegreeHero";
 
 export default function Page() {
   return (
