5be27b114808ce1148f15653a9c940e1d0c51387
Neha Ujjwal <neha.ujjwal@scaler.com>
2025-05-02 17:37:37 +0530

    add(SST): Education Team Section

--- src/app/(sst)/school-of-technology/career-outcomes/page.tsx
+++ src/app/(sst)/school-of-technology/career-outcomes/page.tsx
@@ -4,14 +4,15 @@ import styles from "./page.module.scss";
 import CareerStats from "@modules/sst/career-outcomes/ui/CareerStats/CareerStats";
 import EntrepreneurshipTeam from "@modules/sst/career-outcomes/ui/EntrepreneurshipTeam/EntrepreneurshipTeam";
 import CareerOfficersTeam from "@modules/sst/career-outcomes/ui/CareerOfficersTeam/CareerOfficersTeam";
+import EducationTeam from "@modules/sst/career-outcomes/ui/EducationTeam";
 
 export default function Page() {
-
   return (
     <div className={styles.container}>
       <CareerStats />
       <EntrepreneurshipTeam />
       <CareerOfficersTeam />
+      <EducationTeam />
     </div>
   );
 }
