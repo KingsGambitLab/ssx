88e5c493efd942a804f760ad917e613b0d7c84dd
Neha Ujjwal <neha.ujjwal@scaler.com>
2025-05-02 15:48:57 +0530

    fix: nits

--- src/app/(sst)/school-of-technology/career-outcomes/page.tsx
+++ src/app/(sst)/school-of-technology/career-outcomes/page.tsx
@@ -16,8 +16,8 @@ export default function Page() {
       <Mentorship />
       <SstVsTraditional />
       <ApplyCtaBanner />
-      <Faq />
       <EntrepreneurshipTeam />
+      <Faq />
     </div>
   );
 }
