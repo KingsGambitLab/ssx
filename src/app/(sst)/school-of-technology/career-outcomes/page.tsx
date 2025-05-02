cef95ced332940fa6b335fb332750fedfe762efc
Neha Ujjwal <neha.ujjwal@scaler.com>
2025-05-02 12:31:00 +0530

    add(SST): stats section

--- src/app/(sst)/school-of-technology/career-outcomes/page.tsx
+++ src/app/(sst)/school-of-technology/career-outcomes/page.tsx
@@ -1,9 +1,12 @@
 import styles from "./page.module.scss";
 
+import CareerStats from "@modules/sst/career-outcomes/ui/CareerStats";
+
 export default function Page() {
 
   return (
-    <>
-    </>
+    <div className={styles.container}>
+      <CareerStats />
+    </div>
   );
 }
---
+++ src/modules/sst/career-outcomes/types/index.ts
@@ -0,0 +1,7 @@
+export type CareerStatsCardProps = {
+  title: string;
+  desc: string;
+  image?: string;
+  variant?: "primary" | "tertiary";
+  fullWidth?: boolean;
+}
