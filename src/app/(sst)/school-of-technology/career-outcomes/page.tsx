61bdffe466c7429fb473d479ad65220626b70db7
Neha Ujjwal <neha.ujjwal@scaler.com>
2025-05-02 15:35:28 +0530

    add(SST): entrepreneurship section

--- src/app/(sst)/school-of-technology/career-outcomes/page.tsx
+++ src/app/(sst)/school-of-technology/career-outcomes/page.tsx
@@ -1,12 +1,13 @@
 import styles from "./page.module.scss";
 
-import CareerStats from "@modules/sst/career-outcomes/ui/CareerStats";
-
+import CareerStats from "@modules/sst/career-outcomes/ui/CareerStats/CareerStats";
+import EntrepreneurshipTeam from "@modules/sst/career-outcomes/ui/EntrepreneurshipTeam/EntrepreneurshipTeam";
 export default function Page() {
 
   return (
     <div className={styles.container}>
       <CareerStats />
+      <EntrepreneurshipTeam />
     </div>
   );
 }
---
+++ src/modules/sst/career-outcomes/ui/EntrepreneurshipTeam/EntrepreneurshipTeam.tsx
@@ -0,0 +1,38 @@
+import Section from '@components/common/Section/Section';
+import PeopleCard from '@components/Sst/PeopleCard';
+
+import CarouselWrapper from '@components/common/CarouselWrapper/CarouselWrapper';
+
+import { EntrepreneurshipTeamData } from '../../utils/data';
+
+import styles from './EntrepreneurshipTeam.module.scss';
+
+export default function EntrepreneurshipTeam() {
+  return (
+    <Section section_class="entrepreneurship-team" id="entrepreneurship-team">
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            {EntrepreneurshipTeamData.title}
+          </div>
+          <div className={styles.subTitle}>
+            {EntrepreneurshipTeamData.subTitle}
+          </div>
+        </div>
+        <div className={styles.peoplesWrapper}>
+          <CarouselWrapper slidesToShowInDesktop={3.25} slidesToShowInMobile={1.25}>
+            {EntrepreneurshipTeamData.people.map((person, index) => (
+              <PeopleCard 
+                key={index}
+                image={person?.image?.src} 
+                name={person.name} 
+                prevOrganisation={person.prevOrganisation} 
+                currOrganisation={person.currOrganisation}
+              />
+            ))}
+          </CarouselWrapper>
+        </div>
+      </div>
+   </Section>
+  );
+}
