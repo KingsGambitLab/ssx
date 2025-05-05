ce5d099169822b42c12bf889bae08fabd402384d
Aayush Kumar Shrivastava <aayush.shrivastava@scaler.com>
2025-05-05 16:34:57 +0530

    removed anouncement bar

--- src/app/(sst)/school-of-technology/layout.tsx
+++ src/app/(sst)/school-of-technology/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from 'next';
 
-import { AnnouncementStrip, Header } from "@components/common";
+import { Header } from "@components/common";
 import Navbar from "@components/Sst/Navbar";
 import { LoginModalWrapper } from "@components/Sst/LoginModalWrapper";
 import { METADATA } from "@utils/common/metadata";
-import { ANNOUNCEMENT_STRIP_CONTENT } from "@utils/sst/constants";
 
 import LoginModalProvider from "@context/sst/LoginModalContext";
 import BottomNavbar from "@components/Sst/BottomNavbar";
@@ -16,12 +15,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <LoginModalProvider>
       <Header>
-        <AnnouncementStrip
-          iconSrc={ANNOUNCEMENT_STRIP_CONTENT.iconSrc.src}
-          content={ANNOUNCEMENT_STRIP_CONTENT.content}
-          highlightText={ANNOUNCEMENT_STRIP_CONTENT.highlightText}
-          redirectUrl={ANNOUNCEMENT_STRIP_CONTENT.redirectUrl}
-        />
         <Navbar />
       </Header>
       <main>{children}</main>
