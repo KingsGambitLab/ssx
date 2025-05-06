371c9b87d59985ab8f6ef3452e62baddaf4753d4
Neha Ujjwal <neha.ujjwal@scaler.com>
2025-05-07 01:59:07 +0530

    fix: styling

--- src/modules/sst/career-outcomes/ui/Mentorship/Mentorship.tsx
+++ src/modules/sst/career-outcomes/ui/Mentorship/Mentorship.tsx
@@ -34,35 +34,37 @@ const Mentorship: React.FC = () => {
         <h2>Key Features</h2>
       </div>
       
-      <Row gutter={[24, 24]} className={styles.cardsContainer}>
-        {mentorshipData.map((item, index) => (
-          <Col xs={24} md={12} key={index}>
-            <Card className={styles.mentorCard} bordered={true}>
-              <h3 className={styles.cardTitle}>{item.title}</h3>
-              <p className={styles.cardDescription}>{item.description}</p>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <div className={styles.content}>
+        <Row gutter={[24, 24]} className={styles.cardsContainer}>
+          {mentorshipData.map((item, index) => (
+            <Col xs={24} md={12} key={index}>
+              <Card className={styles.mentorCard} bordered={true}>
+                <h3 className={styles.cardTitle}>{item.title}</h3>
+                <p className={styles.cardDescription}>{item.description}</p>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       
-      <div className={styles.investorSection}>
-        <Image src={LeftQuoteIcon.src} alt="Left Quote" width={24} height={24} className={styles.leftQuote}/>
-        <div className={styles.investorTitle}>
-          <span className={styles.highlight}>What</span> Investors Say
-        </div>
-        <p className={styles.investorQuote}>
-          “Here energy at SST was real. The ideas students shared were grounded in real problems, and with the kind of support and ecosystem they have here, they’re definitely on the right track to building things that matter.”
-        </p>
-        <div className={styles.investorSeparator}/>
-        <div className={styles.footer}>
-          <div className={styles.footerText}>
-            <b>Sidhant Goyal</b> (ex Sequoia Capital, ex McKinsey)
+        <div className={styles.investorSection}>
+          <Image src={LeftQuoteIcon.src} alt="Left Quote" width={24} height={24} className={styles.leftQuote}/>
+          <div className={styles.investorTitle}>
+            <span className={styles.highlight}>What</span> Investors Say
           </div>
-          <button className={styles.linkedinButton} onClick={() => handleOpenLinkedin()}>
-            <Image src={LinkedIcon.src} alt="LinkedIn Icon" width={32} height={32} className={styles.linkedinIcon}/>
-          </button>
+          <p className={styles.investorQuote}>
+            “Here energy at SST was real. The ideas students shared were grounded in real problems, and with the kind of support and ecosystem they have here, they’re definitely on the right track to building things that matter.”
+          </p>
+          <div className={styles.investorSeparator}/>
+          <div className={styles.footer}>
+            <div className={styles.footerText}>
+              <b>Sidhant Goyal</b> (ex Sequoia Capital, ex McKinsey)
+            </div>
+            <button className={styles.linkedinButton} onClick={() => handleOpenLinkedin()}>
+              <Image src={LinkedIcon.src} alt="LinkedIn Icon" width={32} height={32} className={styles.linkedinIcon}/>
+            </button>
+          </div>
+          <Image src={RightQuoteIcon.src} alt="Right Quote" width={24} height={24} className={styles.rightQuote}/>
         </div>
-        <Image src={RightQuoteIcon.src} alt="Right Quote" width={24} height={24} className={styles.rightQuote}/>
       </div>
     </Section>
   );
