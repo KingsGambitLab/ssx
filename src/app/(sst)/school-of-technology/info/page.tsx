30e3ec8bb316ab60afaf88025c27e2716631bb4a
Neha Ujjwal <neha.ujjwal@scaler.com>
2025-05-18 18:56:40 +0530

    fix: nits

--- src/app/(sst)/school-of-technology/info/page.tsx
+++ src/app/(sst)/school-of-technology/info/page.tsx
@@ -2,10 +2,12 @@ import SuccessStories from "@modules/sst/info/ui/SuccessStories";
 import styles from "./page.module.scss";
 import { CareerStats } from "@modules/sst/info/ui/CareerStats";
 import AdmissionStepTimeline from "@modules/sst/info/ui/AdmissionStepTimeline";
+import LoginForm from "@modules/sst/application-form/ui/LoginForm";
 
 export default function Page() {
   return (
     <div className={styles.container}>
+      <LoginForm />
       <div className={styles.mainContainer}>
         <div className={styles.leftContainer}>
           <AdmissionStepTimeline />
