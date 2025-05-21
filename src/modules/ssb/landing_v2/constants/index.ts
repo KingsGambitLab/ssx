4e3235be50de8296cb8dd3bdcfacd1567d0de329
Suryam1260 <suryam.bhukania@scaler.com>
2025-05-21 11:03:51 +0530

    fixed build ssb

--- src/modules/ssb/landing_v2/constants/index.ts
+++ src/modules/ssb/landing_v2/constants/index.ts
@@ -54,7 +54,6 @@ import HinduDesktop from '@public/images/ssb/hindudesktop.webp';
 import BrainfedDesktop from '@public/images/ssb/brainfeeddesktop.webp';
 import OutlookDesktop from '@public/images/ssb/outlookdesktop.webp';
 import TimeDesktop from '@public/images/ssb/timedesktop.webp';
-import TitleHeadline from '@public/images/ssb/news_title.webp';
 import uber_with_frame from '@public/images/ssb/uber_with_frame.webp';
 import mckinsey_with_frame from '@public/images/ssb/mckinsey_with_frame.webp';
 import colgate from '@public/images/ssb/colgate.webp';
